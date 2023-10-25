285d465d1c1c6d0cd6534e02b6aa097011048735
Derian Melvin <derianmelvin88@gmail.com>
2023-10-25 18:33:42 +0700

    docs(AppointmentInfoCard): add comment labels

--- components/riwayat/AppointmentInfoCard.tsx
+++ components/riwayat/AppointmentInfoCard.tsx
@@ -31,11 +31,13 @@ export default function AppointmentInfoCard({
 
   return (
     <div className="w-full flex flex-col gap-2 p-2 border rounded-md bg-slate-200">
+      {/* Appointment Date & Time */}
       <div className="w-full flex items-center justify-between gap-2 text-lg font-medium">
         <span>{getDate}</span>
         <span>{getTime}</span>
       </div>
 
+      {/* Location */}
       <div className="w-full flex items-center justify-between gap-2">
         <div className="flex items-center gap-1">
           <div className="w-5 h-5 flex items-center justify-center rounded-md">
@@ -51,6 +53,7 @@ export default function AppointmentInfoCard({
         </div>
       </div>
 
+      {/* Doctor Image & Name */}
       <div className="w-full flex items-center gap-2 ml-3 my-2">
         <div className="w-14 h-14 flex items-center justify-center rounded-md border">
           <Image
@@ -62,6 +65,7 @@ export default function AppointmentInfoCard({
         <span>{doctor.name}</span>
       </div>
 
+      {/* Appointment Status */}
       <div className="w-full flex items-center justify-end gap-2">
         <span
           className={`w-fit rounded-md font-medium ${
